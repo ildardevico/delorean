f462cd286112a7dffea06b467c69125ba5d5f34e
Kost9in <Kost9inbi4@mail.ru>
2017-03-04 11:40:11 +0200

    Remove config.

--- server/config/index.js
+++ server/config/index.js
@@ -1,10 +1,6 @@
 import config from './config.json';
 
-const envConfig = {
+module.exports =  {
   ...config[process.env.NODE_ENV || 'development'],
-  domain: process.env.NODE_ENV === 'production' ? 'http://social-booster.org' : 'http://localhost:7051',
-  socketPort: 3000,
-  secret: 'delorian-secret',
+  secret: 'delorian-secret'
 };
-
-module.exports = envConfig;
--- src/config/index.js
+++
@@ -1,3 +0,0 @@
-export default {
-  API_HOST: 'localhost:3000'
-}
