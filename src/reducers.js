cf157ca6a1b96da69fee3df1956c10db1149e01c
Kost9in <Kost9inbi4@mail.ru>
2017-03-04 11:22:26 +0200

    Add redux-form to login.

--- src/reducers.js
+++ src/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import { counter } from 'containers/Layout/reducers';
 import { reducer as notifications } from 'react-notification-system-redux';
+import { reducer as formReducer } from 'redux-form';
 
 const reducers = {
   routing,
   counter,
   notifications,
+  form:formReducer,
 };
 
 export default combineReducers(reducers);
