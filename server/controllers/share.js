e07ed9bbafcd6d1e96cbad07881590fe36c8c22e
Kost9in <Kost9inbi4@mail.ru>
2017-03-05 07:15:43 +0200

    Fix duration for download.

--- server/controllers/share.js
+++ server/controllers/share.js
@@ -25,7 +25,11 @@ export const publish = async (req, res) => {
 };
 
 export const download = async (req, res) => {
-  const { fileName, start, duration, type } = req.body;
+  const { fileName, start, type } = req.body;
+
+  let { duration } = req.body;
+  duration = duration - start;
+
   let path;
 
   switch (type) {
